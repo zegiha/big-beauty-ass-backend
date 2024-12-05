b264865fce13a8a4b67211265a98e0c1f9c9ce2e
zegiha <leeseoyul070814@gmail.com>
2024-12-05 09:00:13 +0900

    feat: AuthModule, ConfigModule( env용 )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
